515a1c03076f96a1175d50b456a07d03c474840f
StatefulDecay <35390410+NeillCC@users.noreply.github.com>
2022-10-30 20:53:49 -0400

    Update main.cpp
    broke out main in to functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,63 @@
 #include "DHT.h"
+#include "WiFi.h"
 
 DHT dht;
-float currentTemp = 69.0;
-float currentHumidity = 69.0;
+int dhtPin = 4;
+int sleepSeconds = 5;
+float currentTemp = 0.0;
+float currentHumidity = 0.0;
+char wifiSSID[] = "YoungGuest";
+char wifiPassword[] = "aDayattherange69!";
 
+void startSerial (int baudRate) {
+  Serial.begin(baudRate);
+  Serial.println("");
+  Serial.println("");
+  Serial.println("Booting...");
+}
+void startWiFi(char wifiSSID[], char wifiPassword[]) {
+  WiFi.begin(wifiSSID, wifiPassword);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {delay(1);}
+  Serial.print("Network Status: ");
+  Serial.println(WiFi.status());
+  Serial.print("Connected to WiFi network: ");
+  Serial.println(WiFi.SSID());
+}
+void checkDHT(int dhtPin) {
+  dht.setup(dhtPin);
+  delay(dht.getMinimumSamplingPeriod());
+  currentTemp = dht.toFahrenheit(dht.getTemperature());
+  currentHumidity = dht.getHumidity();
+  //Print temp results for debug
+  Serial.print("Current Temperature: ");
+  Serial.println(currentTemp);
+  Serial.print("Current Humidity: ");
+  Serial.println(currentHumidity);
+}
+void startSleep(int sleepSeconds) {
+  int sleepMilliseconds = sleepSeconds * 1000000;
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+  esp_sleep_enable_timer_wakeup(sleepMilliseconds);
+  WiFi.disconnect();
+  Serial.print("Sleeping for ");
+  Serial.print(sleepSeconds);
+  Serial.println(" seconds");
+  esp_deep_sleep_start();
+}
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  dht.setup(4);
-  Serial.println("Starting...");
+  //Start serial connection
+  startSerial(9600);
+  //Connect to WiFi
+  startWiFi(wifiSSID, wifiPassword);
+  //Check DHT sensor for temperature and humidity
+  checkDHT(dhtPin);
+  //Sleep to save power
+  startSleep(sleepSeconds);    
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    delay(dht.getMinimumSamplingPeriod());
-    currentTemp = dht.toFahrenheit(dht.getTemperature());
-    currentHumidity = dht.getHumidity();
-
-    Serial.println(currentTemp);
-    Serial.println(currentHumidity);
-    //Serial.println(dht.getTemperature());
+    
 }
