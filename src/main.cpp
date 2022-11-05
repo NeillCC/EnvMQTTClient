3f86dd29019538889e8997aa70fc6a3445ed1fc4
StatefulDecay <35390410+NeillCC@users.noreply.github.com>
2022-11-05 14:59:23 -0400

    hostname change
    changed hostname to default to mac address

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,26 @@
 
 #pragma region User Settings
 //WiFi
-char wifiSSID[] = "iot7"; //WiFi Name
-char wifiPassword[] = "aDayattherange69!"; //WiFi Password
-char wifiHostname[] = "tempProbe01"; //Name to give dhcp server. This just needs be unique to your MQTT broker
+char wifiSSID[] = "iot7";                   //WiFi Name
+char wifiPassword[] = "aDayattherange69!";  //WiFi Password
+char wifiCustomHostname[] = "";  //Name to give dhcp server. This just needs be unique to your MQTT broker. Defaults to mac address
+bool useMacForHostname = true;              //Whether to use MAC Address as hostname. Must change to false for wifiCustomHostname to matter
 //MQTT Settings
-char mqttServer[] = "10.0.1.11"; //DNS or IP for MQTT Broker
-int  mqttPort = 2883; //Port for MQTT Broker. 1883 is standard
+char mqttServer[] = "10.0.1.11";            //DNS or IP for MQTT Broker
+int  mqttPort = 2883;                       //Port for MQTT Broker. 1883 is standard
 //Power Settings
 //TODO sleepSeconds can be overriden by declaring a cycleTime topic on your MQTT Broker
-int sleepSeconds = 5; //How often to boot and update MQTT Broker. Higher number means less frequent updates, but better battery life. Default 60
+int sleepSeconds = 60;                       //How often to boot and update MQTT Broker. Higher number means less frequent updates, but better battery life. Default 60
 #pragma endregion
 
 
-#pragma region declaration
+#pragma region declaration                  //Global Variables
 DHT dht;
 WiFiClient wifiClient;
 PubSubClient mqttClient;
 #pragma endregion
 #pragma region DHT Variables
-int dhtPin = 4;
+int dhtPin = 4;                             //DHT connection pin
 float currentTemperatureFarnehit;
 float currentTemperatureCelsius;
 float currentHumidity = 0.0;
@@ -37,8 +38,18 @@ void startSerial (int baudRate) {
   Serial.println("");
   Serial.println("Booting...");
 }
-void startWiFi(char wifiSSID[], char wifiPassword[]) {  
+std::string startWiFi(char wifiSSID[], char wifiPassword[], bool useMacForHostname = true, std::string hostname = wifiCustomHostname) {  
   Serial.println("Attempting WiFi connection...");
+  int customName = strlen(wifiCustomHostname);
+  Serial.println(customName);
+  switch (customName != 0) {
+    case true:
+      WiFi.setHostname(hostname.c_str());
+      break;
+    default:
+      WiFi.setHostname(WiFi.macAddress().c_str());
+      break;
+  } 
   WiFi.begin(wifiSSID, wifiPassword);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {delay(1);}
   Serial.println();
@@ -46,6 +57,8 @@ void startWiFi(char wifiSSID[], char wifiPassword[]) {
   Serial.println(WiFi.status());
   Serial.print("Connected to WiFi network: ");
   Serial.println(WiFi.SSID());
+  Serial.print("Hostname: ");
+  Serial.println(WiFi.getHostname());
   Serial.print("IPv4: ");
   Serial.println(WiFi.localIP());
   Serial.print("Netmask: ");
@@ -55,10 +68,12 @@ void startWiFi(char wifiSSID[], char wifiPassword[]) {
   Serial.print("DNS: ");
   Serial.println(WiFi.dnsIP());
   Serial.print("Can resolve and reach google? [bool]: ");
-  Serial.println(wifiClient.connect("google.com", 80)); //Check local DNS for connectivity
+  Serial.println(wifiClient.connect("google.com", 80));
   wifiClient.stop();
   Serial.println();
-
+  
+  return WiFi.getHostname();
+  
 }
 void checkDHT(int dhtPin) {
   dht.setup(dhtPin);
@@ -76,12 +91,14 @@ void checkDHT(int dhtPin) {
   Serial.println(currentTemperatureFarnehit);
   Serial.print("Current Temperature C: ");
   Serial.println(currentTemperatureCelsius);
-  Serial.print("Current Humidity: ");
+  Serial.print("Current Humidity %: ");
   Serial.println(currentHumidity);
 }
-void startMQTT(WiFiClient wifiClient, PubSubClient mqttClient, char mqttUsername[] = NULL, char mqttPassword[] = NULL) {
+void startMQTT(WiFiClient wifiClient, PubSubClient mqttClient, std::string hostname, char mqttUsername[] = NULL, char mqttPassword[] = NULL) {
   //Supports MQTT auth by disabled by default
   //wifiClient.connect(mqttServer, mqttPort);
+  char wifiHostnameChar[hostname.length() + 1];
+  strcpy(wifiHostnameChar, hostname.c_str());
   mqttClient.setClient(wifiClient);
   mqttClient.setServer(mqttServer, mqttPort);
 
@@ -95,24 +112,23 @@ void startMQTT(WiFiClient wifiClient, PubSubClient mqttClient, char mqttUsername
     Serial.println(i);
     
     // Attempt to connect
-    if (mqttClient.connect(wifiHostname, mqttUsername, mqttPassword)) {
+    if (mqttClient.connect(wifiHostnameChar, mqttUsername, mqttPassword)) {
       Serial.print("MQTT connection to ");
       Serial.print(mqttServer);
       Serial.println(" succesful!");
       //Build MQTT Topic char[]
       std::string tempFTxt = "/Farenheit";
       std::string tempCTxt = "/Celsius";
-      std::string humidTxt = "/Humidity";
-      std::string mqttName = wifiHostname;
-      int l1 = mqttName.length() + tempFTxt.length();
-      int l2 = mqttName.length() + tempCTxt.length();
-      int l3 = mqttName.length() + humidTxt.length();
+      std::string humidTxt = "/HumidityPercent";
+      int l1 = hostname.length() + tempFTxt.length();
+      int l2 = hostname.length() + tempCTxt.length();
+      int l3 = hostname.length() + humidTxt.length();
       char farenheitTopic[l1 + 1];
       char celsiusTopic[l2 + 1];
       char humidityTopic[l3 + 1];
-      std::string tempF = wifiHostname + tempFTxt;
-      std::string tempC = wifiHostname + tempCTxt;
-      std::string humid = wifiHostname + humidTxt;
+      std::string tempF = wifiHostnameChar + tempFTxt;
+      std::string tempC = wifiHostnameChar + tempCTxt;
+      std::string humid = wifiHostnameChar + humidTxt;
       strcpy(farenheitTopic, tempF.c_str());
       strcpy(celsiusTopic, tempC.c_str());
       strcpy(humidityTopic, humid.c_str());
@@ -148,11 +164,11 @@ void setup() {
   //Start serial connection
   startSerial(9600);
   //Connect to WiFi
-  startWiFi(wifiSSID, wifiPassword);
+  std::string hostname = startWiFi(wifiSSID, wifiPassword);
   //Check DHT sensor for temperature and humidity
   checkDHT(dhtPin);
   //Publish DHT data to MQTT broker
-  startMQTT(wifiClient, mqttClient);
+  startMQTT(wifiClient, mqttClient, hostname);
   //Sleep to save power
   startSleep(sleepSeconds);    
 }
